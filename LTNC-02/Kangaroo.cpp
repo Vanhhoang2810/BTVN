f579098052c59f72653cb5e939a77b298d48692b
Vanhhoang2810 <160811741+Vanhhoang2810@users.noreply.github.com>
2024-04-09 21:11:54 +0700

    Update Kangaroo.cpp

--- LTNC-02/Kangaroo.cpp
+++ LTNC-02/Kangaroo.cpp
@@ -18,7 +18,13 @@ vector<string> split(const string &);
  */
 
 string kangaroo(int x1, int v1, int x2, int v2) {
-
+    if ((x1 < x2 && v1 <= v2) || (x1 > x2 && v1 >= v2)) {
+        return "NO";
+    } else if ((x1 - x2) % (v2 - v1) == 0) {
+        return "YES";
+    } else {
+        return "NO";
+    }
 }
 
 int main()
